19c110559e2bb8d09ec35f2dda00679e763d05dc
Wojciech Malarski <w.malarski@gmail.com>
2024-05-28 21:29:04 +0200

    adds login page

--- src/components/Link/Link.ts
+++ src/components/Link/Link.ts
@@ -1,20 +1,21 @@
 import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import { buttonRecipe, buttonStyles, type ButtonProps } from "./Button.styles";
+import {
+	buttonRecipe,
+	buttonStyles,
+	type ButtonProps,
+} from "../Button/Button.styles";
 
-@customElement("alb-button")
-export class AlbButton extends LitElement {
+@customElement("alb-anchor")
+export class AlbAnchor extends LitElement {
 	@property({ type: String, reflect: true })
 	variant: ButtonProps["variant"] = "primary";
 
 	@property({ type: String, reflect: true })
 	size: ButtonProps["size"] = "medium";
 
-	@property({ type: String })
-	type: "button" | "submit" | "reset" = "button";
-
-	@property({ type: Boolean, reflect: true })
-	disabled = false;
+	@property({ type: String, reflect: true })
+	href: string | undefined;
 
 	static override styles = [buttonStyles];
 
@@ -22,7 +23,7 @@ export class AlbButton extends LitElement {
 		return html`<button class=${buttonRecipe({
 			size: this.size,
 			variant: this.variant,
-		})} type=${this.type} disabled=${this.disabled}>
+		})} href=${this.href}>
       <slot></slot>
     </button>`;
 	}
---
+++ src/pages/login/mastodon/index.ts
@@ -0,0 +1,8 @@
+import type { APIContext } from "astro";
+import { createAuthorizationUrl } from "@server/auth";
+
+export const GET = async (context: APIContext): Promise<Response> => {
+	const url = await createAuthorizationUrl(context);
+
+	return context.redirect(url.toString());
+};
