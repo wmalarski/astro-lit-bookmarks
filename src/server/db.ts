e7688eef3da4aebc3e53f6c742ae9fad6ae1b719
Wojciech Malarski <w.malarski@gmail.com>
2024-05-30 12:52:36 +0200

    adds new bookmark sqlite tables

--- src/server/db.ts
+++ src/server/db.ts
@@ -19,3 +19,33 @@ export const sessionTable = sqliteTable("session", {
 	expiresAt: integer("expires_at").notNull(),
 	accessToken: text("access_token").notNull(),
 });
+
+export const bookmarkTable = sqliteTable("bookmark", {
+	id: text("id").notNull().primaryKey(),
+	userId: text("user_id")
+		.notNull()
+		.references(() => userTable.id),
+	content: text("content"),
+	mastoBookmarkId: text("masto_bookmark_id"),
+	priority: integer("priority").default(0).notNull(),
+	done: integer("done", { mode: "boolean" }).default(false).notNull(),
+	createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+});
+
+export const tagTable = sqliteTable("tag", {
+	id: text("id").notNull().primaryKey(),
+	name: text("name").notNull(),
+});
+
+export const bookmarkTagTable = sqliteTable("bookmark_tag", {
+	id: text("id").notNull().primaryKey(),
+	userId: text("user_id")
+		.notNull()
+		.references(() => userTable.id),
+	tagId: text("tag_id")
+		.notNull()
+		.references(() => tagTable.id),
+	bookmarkId: text("bookmark_id")
+		.notNull()
+		.references(() => bookmarkTable.id),
+});
