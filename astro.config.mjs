2c304ed0358b16c7b5a234f9a5642a66f4e6fc85
Wojciech Malarski <w.malarski@gmail.com>
2024-09-03 20:46:11 +0200

    removes lit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,63 +1,69 @@
-import { defineConfig } from "astro/config";
-import AstroPWA from "@vite-pwa/astro";
-import lit from "@astrojs/lit";
 import node from "@astrojs/node";
+import AstroPWA from "@vite-pwa/astro";
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [lit(), AstroPWA({
-    // mode: "development",
-    // base: "/",
-    // scope: "/",
-    includeAssets: ["favicon.svg"],
-    // registerType: "autoUpdate",
-    manifest: {
-      name: "Lit Bookmarks",
-      short_name: "Lit Bookmarks",
-      theme_color: "#afffff",
-      start_url: "/",
-      background_color: "#eeeeee",
-      display: "standalone",
-      description: "Lit Bookmarks",
-      orientation: "portrait",
-      display_override: ["standalone"],
-      icons: [{
-        src: "pwa-192x192.png",
-        sizes: "192x192",
-        type: "image/png"
-      }, {
-        src: "pwa-512x512.png",
-        sizes: "512x512",
-        type: "image/png"
-      }, {
-        src: "pwa-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable"
-      }],
-      share_target: {
-        action: "/share-target/",
-        enctype: "multipart/form-data",
-        method: "POST",
-        params: {
-          title: "title",
-          text: "text",
-          url: "url"
-        }
-      }
-    }
-    // workbox: {},
-    // devOptions: {
-    // 	enabled: true,
-    // },
-  }), tailwind()],
+  integrations: [
+    AstroPWA({
+      // mode: "development",
+      // base: "/",
+      // scope: "/",
+      includeAssets: ["favicon.svg"],
+      // registerType: "autoUpdate",
+      manifest: {
+        name: "Lit Bookmarks",
+        short_name: "Lit Bookmarks",
+        theme_color: "#afffff",
+        start_url: "/",
+        background_color: "#eeeeee",
+        display: "standalone",
+        description: "Lit Bookmarks",
+        orientation: "portrait",
+        display_override: ["standalone"],
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        share_target: {
+          action: "/share-target/",
+          enctype: "multipart/form-data",
+          method: "POST",
+          params: {
+            title: "title",
+            text: "text",
+            url: "url",
+          },
+        },
+      },
+      // workbox: {},
+      // devOptions: {
+      // 	enabled: true,
+      // },
+    }),
+    tailwind(),
+  ],
   experimental: {
-    actions: true
+    actions: true,
   },
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "standalone",
+  }),
 });
--- src/styles/tailwind.ts
+++
@@ -1,5 +0,0 @@
-import { unsafeCSS } from "lit";
-
-import style from "./global.css?inline";
-
-export const tailwindStyles = unsafeCSS(style);
